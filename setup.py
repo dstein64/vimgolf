13cff360cbd6eb72cff89648c552a0939cc96155
Dan <dstein64@users.noreply.github.com>
2019-02-23 01:20:40 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,36 @@
+import os
+from setuptools import setup
+
+version_txt = os.path.join(os.path.dirname(__file__), 'vimgolf', 'version.txt')
+with open(version_txt, 'r') as f:
+    version = f.read().strip()
+
+setup(
+    author='Daniel Steinberg',
+    author_email='ds@dannyadam.com',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: MacOS',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: Unix',
+        'Programming Language :: Python :: 3',
+        'Topic :: Text Editors',
+        'Topic :: Games/Entertainment',
+    ],
+    description='A vimgolf client written in Python',
+    keywords=['vim', 'vimgolf'],
+    license='MIT',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    name='vimgolf',
+    package_data={'vimgolf': ['version.txt', 'vimgolf.vimrc']},
+    packages=['vimgolf'],
+    python_requires='>=3.5',
+    scripts=['bin/vimgolf'],
+    url='https://github.com/dstein64/vimgolf',
+    version=version,
+)
