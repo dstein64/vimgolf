95b054133dc5286af1e73bdb891c9e331383dbbb
Dan <dstein64@users.noreply.github.com>
2022-05-22 20:17:14 -0400

    Move test.

--- tests/test_vimgolf.py
+++ tests/test_vimgolf.py
@@ -59,9 +59,6 @@ class TestVimgolf(unittest.TestCase):
         # Use the challenge with fewest entries to avoid VimGolf Issue #306.
         #   https://github.com/igrigorik/vimgolf/issues/306
         entry = min((x, idx) for idx, x in enumerate(entry_counts))[1] + 1
-        # A workaround was added to avoid a cp1252 encoding exception on Windows
-        # GitHub Actions for the following challenge.
-        self.assertEqual(main(['vimgolf', 'show', '4d1c7ee635b40650b8000203']), 0)
         self.assertEqual(main(['vimgolf', 'show', '+{}'.format(entry)]), 0)
         # The following ID is for 'Pascal's Triangle'
         # http://www.vimgolf.com/challenges/5ca2bc786b547e000c77fd52
@@ -71,6 +68,9 @@ class TestVimgolf(unittest.TestCase):
         # issue without the workaround.
         # http://www.vimgolf.com/challenges/55b18bbea9c2c30d04000001
         self.assertEqual(main(['vimgolf', 'show', '55b18bbea9c2c30d04000001']), 0)
+        # A workaround was added to avoid a cp1252 encoding exception on Windows
+        # GitHub Actions for the following challenge.
+        self.assertEqual(main(['vimgolf', 'show', '4d1c7ee635b40650b8000203']), 0)
 
 
     def test_ruby_client_version(self):
